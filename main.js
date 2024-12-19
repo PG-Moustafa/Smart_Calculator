110bd09cfe02972f85a6d5ff6e4179a8ec850e51
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2024-12-19 20:39:23 +0200

    Prefix function implemented with js

--- main.js
+++ main.js
@@ -18,24 +18,25 @@ const divides = document.getElementById("divides")
 const equals = document.getElementById("equal")
 const clear = document.getElementById("clear")
 
-let expression = ""
-let result = document.getElementById("result")
+let expression = []
+let result = 0
+let display_result = document.getElementById("result")
 let choice = document.getElementById("choice")
 
 function read_input() {
-    zero.addEventListener("click", () => add_to_expression("0"))
-    one.addEventListener("click", () => add_to_expression("1"))
-    two.addEventListener("click", () => add_to_expression("2"))
-    three.addEventListener("click", () => add_to_expression("3"))
-    four.addEventListener("click", () => add_to_expression("4"))
-    five.addEventListener("click", () => add_to_expression("5"))
-    six.addEventListener("click", () => add_to_expression("6"))
-    seven.addEventListener("click", () => add_to_expression("7"))
-    eight.addEventListener("click", () => add_to_expression("8"))
-    nine.addEventListener("click", () => add_to_expression("9"))
+    zero.addEventListener("click", () => add_to_expression(0))
+    one.addEventListener("click", () => add_to_expression(1))
+    two.addEventListener("click", () => add_to_expression(2))
+    three.addEventListener("click", () => add_to_expression(3))
+    four.addEventListener("click", () => add_to_expression(4))
+    five.addEventListener("click", () => add_to_expression(5))
+    six.addEventListener("click", () => add_to_expression(6))
+    seven.addEventListener("click", () => add_to_expression(7))
+    eight.addEventListener("click", () => add_to_expression(8))
+    nine.addEventListener("click", () => add_to_expression(9))
 
-    plus.addEventListener("click", () => add_to_expression("+"))
-    minus.addEventListener("click", () => add_to_expression("-"))
+    plus.addEventListener("click", () => add_to_expression('+'))
+    minus.addEventListener("click", () => add_to_expression('-'))
     times.addEventListener("click", () => add_to_expression("*"))
     divides.addEventListener("click", () => add_to_expression("/"))
 
@@ -44,34 +45,95 @@ function read_input() {
 }
 
 function add_to_expression (n) {
-    expression += n
-    result.innerHTML = expression
+    expression.push(n)
+    let str = ""
+    for (element in expression) {
+        str += element
+    }
+    display_result.innerHTML = str
 }
 
 function clear_expression() {
-    expression = ""
-    result.innerHTML = ""
+    expression = []
+    display_result.innerHTML = ""
+}
+
+function print_result () {
+    display_result.innerHTML = result
+    result = 0
 }
 
 function calculate_expression() {
     if (choice == "Infix") 
     {
+        result = calculate_infix_expression()
         return
     } 
     else if (choice == "Prefix")
     {
+        result = calculate_prefix_expression()
+        print_result()
         return
     } 
     else if (choice == "Postfix") 
     {
+        result = calculate_postfix_expression()
         return
     }
 }
 
-function calculate_prefix_expression() {
+// 
+function calculate_infix_expression() {
 
 }
 
+///
+function calculate_prefix_expression() {
+    let result = 0
+    let numbers = []
+
+    for (element in expression) {
+        if (0 <= element <= 9)
+            numbers.push(element)
+
+        else 
+        {
+            n1 = numbers.pop()
+            n2 = numbers.pop()
+
+            switch (element) {
+                case '+':
+                    if (n2 != null)
+                        result += (n1+n2)
+                    else
+                        result += (n1)
+                case '-':
+                    if (n2 != null)
+                        result += (n1-n2)
+                    else
+                        result -= (n1)
+                case '*':
+                    if (n2 != null)
+                        result += (n1*n2)
+                    else
+                        result *= (n1)
+                case '/':
+                    if (n2 != null)
+                        result += (n1/n2)
+                    else
+                        result /= (n1)
+            }
+        }
+        
+    }
+    return result
+
+}
+
+//
+function calculate_postfix_expression() {
+
+}
 
 
 read_input()
